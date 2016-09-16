84769a11049c2dc97eb065de3d7fb78f4b363bde
deivan <ariastation@ua.fm>
2016-09-16 19:57:17 +0300

    letsexplore request object

--- lesson03/app.js
+++ lesson03/app.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (req,res) {
+  console.log('it is request:', req); // headers method url
+  //console.log('it is response:', res);
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Slava Ukraine! Heroyam Slava!');
 });
