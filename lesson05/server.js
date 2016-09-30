9add96426844305881ea205deeac09943e69c5e4
deivan <ariastation@ua.fm>
2016-09-30 16:23:53 +0300

    Added menu functionality

--- lesson05/server.js
+++ lesson05/server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
     appData.visitors++;
     appData.userAgents.push(req.headers['user-agent']);
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/main.html');
 });
 
 app.get('/news', function (req, res) {
