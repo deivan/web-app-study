73be18138e483afdb25c9459f0c1a822469b9aa0
deivan <ariastation@ua.fm>
2016-10-22 11:45:48 +0300

    Fixed styles

--- lesson08/public/js/app.js
+++ lesson08/public/js/app.js
@@ -2,12 +2,10 @@ angular.module('app',['ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        template : '<h1>Mainpage</h1>',
-        controller  : 'mainPage'
+        template : '<h1>Mainpage</h1>'
       })
       .when('/equipment', {
-        template : '<h1>Eqquipment page</h1>',
-        controller  : 'mainPage'
+        template : '<h1>Eqquipment page</h1>'
       })
   })
   .controller('mainPage', function ($scope) {
