0030450435a27dc6dd89dfe5ddced825c4bd0e63
deivan <ariastation@ua.fm>
2016-12-02 20:54:04 +0200

    Added message viewer

--- lesson13/public/js/app.js
+++ lesson13/public/js/app.js
@@ -18,8 +18,35 @@ angular.module('app',['ngRoute'])
       })
   })
   
-  .controller('mainPage', function ($scope, $rootScope, $http) {
+  .controller('mainPage', function ($scope, $rootScope) {
     $rootScope.profile = {};
+    $rootScope.me = 'Adam'; //$rootScope.profile.username;
+    $rootScope.conversations = [
+      {
+        _id: 1,
+        authors: ['Snake', 'Adam'],
+        messages: [
+          { date: '2016-11-29 09:10', author: 'Adam', text: 'Hello, how R U?'},
+          { date: '2016-11-29 09:12', author: 'Snake', text: 'tnx im 5n'}
+        ]
+      },
+      {
+        _id: 2,
+        authors: ['Eve', 'Adam'],
+        messages: [
+          { date: '2016-11-30 10:15', author: 'Adam', text: 'Hello, Eve, it is Adam.Do you wanna an apple?'},
+          { date: '2016-11-30 12:15', author: 'Eve', text: 'mmmmm... are you about macbook?'},
+          { date: '2016-11-30 13:10', author: 'Adam', text: 'stupid blondy i am about meet for you!!!'}
+        ]
+      }
+    ];
+    $rootScope.getMate = function (authors) {
+      if (authors[0] == $rootScope.me) {
+        return authors[1];
+      } else {
+        return authors[0];
+      }
+    };
   })
   
   .controller('ProfilePage', function ($scope, $rootScope, $http) {
@@ -52,42 +79,24 @@ angular.module('app',['ngRoute'])
     };
   })
   
-  .controller('MessagesPage', function ($scope) {
-    var me = 'Adam'; //$rootScope.profile.username;
+  .controller('MessagesPage', function ($scope, $rootScope) {
+    
     $scope.users = [
       {username:'Adam'},
       {username:'Eve'},
       {username:'Snake'}
     ];
-    $scope.conversations = [
-      {
-        _id: 1,
-        authors: ['Snake', 'Adam'],
-        messages: [
-          { date: '2016-11-29 09:10', author: 'Adam', text: 'Hello, how R U?'},
-          { date: '2016-11-29 09:12', author: 'Snake', text: 'tnx im 5n'}
-        ]
-      },
-      {
-        _id: 2,
-        authors: ['Eve', 'Adam'],
-        messages: [
-          { date: '2016-11-30 10:15', author: 'Adam', text: 'Hello, Eve, it is Adam.Do you wanna an apple?'},
-          { date: '2016-11-30 12:15', author: 'Eve', text: 'mmmmm... are you about macbook?'},
-          { date: '2016-11-30 13:10', author: 'Adam', text: 'stupid blondy i am about meet for you!!!'}
-        ]
-      }
-    ];
     
-    $scope.getMate = function (authors) {
-      if (authors[0] == me) {
-        return authors[1];
-      } else {
-        return authors[0];
-      }
-    };
   })
   
-  .controller('MessageViewPage', function ($scope) {
-    
+  .controller('MessageViewPage', function ($scope, $rootScope, $routeParams) {
+    var id = $routeParams.id;
+    $scope.conversation = {};
+    for (var i = 0; i < $rootScope.conversations.length; i++) {
+      if ($rootScope.conversations[i]._id == id) {
+        $scope.conversation = $rootScope.conversations[i];
+        console.log('conv',$scope.conversation)
+      }
+    }
+      
   });
